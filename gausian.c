bdd7a2ee1c61ce5bcd307888d514bef0514356f8
Timothy Flavin <BinaryOwlizard@gmail.com>
2021-12-21 17:23:01 -0600

    Added abulity to stop debug output and also commented gausian elimination

--- gausian.c
+++ gausian.c
@@ -55,7 +55,17 @@ void print_mat(int r, int lc, int rc, double* mat){
   }
 }
 
-void g_elim(int rows, int l_cols, int r_cols, double* mat){
+// Performas gausian elimination and then back substitution
+// by adding the solved rows back to the original rows
+// 1 2 =  1
+// 2 3 =  1
+// ----------------------
+// 1 2 = 1
+// 0 1 = 1
+//
+// 1 0 = -1
+// 0 1 = 1
+void g_elim(int rows, int l_cols, int r_cols, double* mat, bool debug=false){
   if(l_cols>rows){
     printf("Not implemented yet, needs to have l_cols<=rows");
     return;
@@ -65,51 +75,51 @@ void g_elim(int rows, int l_cols, int r_cols, double* mat){
   for(int c=0; c<l_cols-1; c++){
     int pivot=-1, r=c;
     while(r<rows && pivot<0){
-      printf("Testing %i,%i for pivot: %.3f\n",r,c,mat[r*cols+c]);
+      if(debug) printf("Testing %i,%i for pivot: %.3f\n",r,c,mat[r*cols+c]);
       if(mat[r*cols+c]!=0.0){
         pivot = r;
-        printf("%i Pivot row found\n",pivot);
+        if(debug) printf("%i Pivot row found\n",pivot);
       }
       r++;
     }
     if(pivot==-1)
       return;
     if(pivot!=c){
-      printf("pivot: r%i != c: r%i, so swapping pivot and c\n",pivot,c);
+      if(debug) printf("pivot: r%i != c: r%i, so swapping pivot and c\n",pivot,c);
       swap_r(pivot,c,mat,l_cols,r_cols);
-      print_mat(rows, l_cols, r_cols, mat);
+      if(debug) print_mat(rows, l_cols, r_cols, mat);
     }
     for(r=c+1; r<rows; ++r){
       double a = -(mat[r*cols+c]/mat[c*cols+c]);
-      printf("adding %.3f*r%i to r%i\n",a,c,r);
+      if(debug) printf("adding %.3f*r%i to r%i\n",a,c,r);
       add_r(c, r, a, mat, l_cols, r_cols);
-      print_mat(rows, l_cols, r_cols, mat);
+      if(debug) print_mat(rows, l_cols, r_cols, mat);
     }
-    printf("Scaling pivot row to 1 p: %.3f, c: %.3f, a: %.3f\n",pivot,c, 1/mat[c*cols+c]);
+    if(debug) printf("Scaling pivot row to 1 p: %.3f, c: %.3f, a: %.3f\n",pivot,c, 1/mat[c*cols+c]);
     scale_r(c, 1/mat[c*cols+c], mat, l_cols, r_cols);
-    print_mat(rows, l_cols, r_cols, mat);
+    if(debug) print_mat(rows, l_cols, r_cols, mat);
     
-    printf("---------------------------------------\n");
+    if(debug) printf("---------------------------------------\n");
   }
   if(mat[(rows-1)*cols+(rows-1)] != 0.0){
-    printf("Scaling final row to 1");
+    if(debug) printf("Scaling final row to 1");
     scale_r(rows-1, 1/mat[(rows-1)*cols+(rows-1)], mat, l_cols, r_cols);
-    print_mat(rows, l_cols, r_cols, mat); 
-    printf("---------------------------------------\n");
+    if(debug) print_mat(rows, l_cols, r_cols, mat); 
+    if(debug) printf("---------------------------------------\n");
   }
   
   for(int c=l_cols-1; c>0; --c){
     if(mat[c*cols+c]==0){
-      printf("r%i diagonal is zero. skipping it...");
+      if(debug) printf("r%i diagonal is zero. skipping it...");
       continue;
     }
     for(int r=c-1; r>-1; --r){
       double a = -(mat[r*cols+c]/mat[c*cols+c]);
-      printf("adding %.3f*r%i to r%i\n",a,c,r);
+      if(debug) printf("adding %.3f*r%i to r%i\n",a,c,r);
       add_r(c, r, a, mat, l_cols, r_cols);
-      print_mat(rows, l_cols, r_cols, mat);
+      if(debug) print_mat(rows, l_cols, r_cols, mat);
     }
-    printf("---------------------------------------\n");
+    if(debug) printf("---------------------------------------\n");
   }  
 }
 
@@ -119,8 +129,9 @@ int main(int argc, char** argv) {
   //for(int i=0; i<12; ++i){
     //mat[i]=i+1;
   //}
-  double m[12] = {0,1,-1,3,-2,4,-1,1,-2,5,-4,-2};
+  double m[12] = {1,1,1,1,1,2,4,2,1,3,9,0};
   //mat=m;
   //mat[0]=4.0;
   g_elim(r, 3, 1, m);
+  print_mat(r, 3, 1, m);
 }
